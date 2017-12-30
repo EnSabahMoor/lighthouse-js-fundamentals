d42b76bf01cd02f67a4c1f618c7d41a593d0278d
Andray Domise <andreanthony@outlook.com>
2017-12-30 15:17:02 -0500

    Added 'for' exercise called 'packing-list.js'

---
+++ packing-list.js
@@ -0,0 +1,8 @@
+var packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+packingList.push("cookbooks");
+
+console.log("Kitchen stuff to pack:");
+
+for (var i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
