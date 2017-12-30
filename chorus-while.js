e618a719426997cd11b7da82ea1e73dc3b2c2ef2
Andray Domise <andreanthony@outlook.com>
2017-12-30 15:02:47 -0500

    Modified chorus-while to add key change

--- chorus-while.js
+++ chorus-while.js
@@ -1,7 +1,10 @@
 var chorus = "Let's dance!";
 var repeat = 0;
 while (repeat < 10) {
-  console.log(chorus);
-  repeat++;
+ if (repeat === 5) {
+    console.log("*change key*");
+  }
+ console.log(chorus);
+ repeat++;
 }
 console.log("Until the sun comes up!");
